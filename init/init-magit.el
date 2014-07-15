1a68f9ceaea4c5500472e4200a1a3ada57c01865
Yemin Zhang <eyemzha@esekilxxen1712.rnd.ericsson.se>
2014-07-15 16:03:20 +0200

    magit: some improvements for magit
    1. stage and unstage without comfirmation
    2. C-c C-a to just amend a commit without changing commit msg.

--- init/init-magit.el
+++ init/init-magit.el
@@ -3,6 +3,18 @@
 ;; reservered for future use when emacs 24.4 is ready
 ;;(require 'magit-filenotify)
 
+(set-default 'magit-stage-all-confirm nil)
+(set-default 'magit-unstage-all-confirm nil)
+
+;; C-c C-a to amend without any prompt
+(defun magit-just-amend ()
+  (interactive)
+  (save-window-excursion
+    (shell-command "git --no-pager commit --amend --reuse-message=HEAD")
+    (magit-refresh)))
+
+(eval-after-load "magit"
+  '(define-key magit-status-mode-map (kbd "C-c C-a") 'magit-just-amend))
 
 (eval-after-load 'info
   '(progn (info-initialize)
