4b5d2bbdb653de31a24bec4abcc1e259d2cce856
Yemin Zhang <eyemzha@esekilxxen1712.rnd.ericsson.se>
2014-07-17 15:37:19 +0200

    jump to function definition in erlang mode
    use etags. jump to the function under point.
    
    In the future, etags file should be configurable
    in the env.el

--- init/init-erlang.el
+++ init/init-erlang.el
@@ -22,8 +22,19 @@
 (shell-command "scp ~/dmx/dmxc/out/opt/dmxc/lib/erlang/lib/dmxc-4.0/ebin/* root@SC-2-1:/persistent/dmx/active/dmxc/dev_patches/" nil)
 (erl-reload-modules "dmxc1@SC-2-1"))
 
+(defun erlang-find-tag-under-point ()
+(interactive)
+(erlang-find-tag (erlang-find-tag-default))
+)
+
 (add-hook 'erlang-mode-hook #'(lambda ()
 (define-key erlang-mode-map (kbd "<f5>") 'dmx-compile)))
 ;;(define-key erlang-mode-map (kbd "<f6>") 'dmx-reload)
+
+(add-hook 'erlang-mode-hook #'(lambda ()
+(define-key erlang-mode-map (kbd "M-.") 'erlang-find-tag-under-point)))
+
+(add-hook 'erlang-mode-hook #'(lambda ()
+(define-key erlang-mode-map (kbd "M-,") 'pop-tag-mark)))
 
 (provide 'init-erlang)
