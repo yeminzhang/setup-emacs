35ae53fee6fa21ad1399e439cca5288cbd117248
Yemin Zhang <yemin.zhang@ericsson.com>
2015-01-09 22:55:49 +0100

    Show full file-path in helm result

--- init/init-edit.el
+++ init/init-edit.el
@@ -53,125 +53,8 @@
         (setq str (replace-match "" t t str)))
       str)
 
-
-(defun my-helm-locate-init ()
-  "Initialize async locate process for `helm-source-locate'."
-  (let* ((locate-is-es (string-match "^es" helm-locate-command))
-         (real-locate (string-match "^locate" helm-locate-command))
-         (case-sensitive-flag (if locate-is-es "-i" ""))
-         (ignore-case-flag (if (or locate-is-es
-                                   (not real-locate)) "" "-i"))
-         process-connection-type)
-    (prog1
-        (start-process-shell-command
-         "locate-process" helm-buffer
-         (format helm-locate-command
-                 (case helm-locate-case-fold-search
-                   (smart (let ((case-fold-search nil))
-                            (if (string-match "[A-Z]" helm-pattern)
-                                case-sensitive-flag
-                                ignore-case-flag)))
-                   (t (if helm-locate-case-fold-search
-                          ignore-case-flag
-                          case-sensitive-flag)))
-				 (replace-regexp-in-string " " ".*" args)))
-      (set-process-sentinel
-       (get-process helm-buffer)
-       #'(lambda (process event)
-           (if (string= event "finished\n")
-               (with-helm-window
-                 (setq mode-line-format
-                       '(" " mode-line-buffer-identification " "
-                         (line-number-mode "%l") " "
-                         (:eval (propertize
-                                 (format "[Locate Process Finish- (%s results)]"
-                                         (max (1- (count-lines
-                                                   (point-min) (point-max))) 0))
-                                 'face 'helm-grep-finish))))
-                 (force-mode-line-update))
-               (helm-log "Error: Locate %s"
-                         (replace-regexp-in-string "\n" "" event))))))))
-
-
-
-(defun my-new-helm-locate-init ()
-  "Initialize async locate process for `helm-source-locate'."
-  (let* ((locate-is-es (string-match "\\`es" helm-locate-command))
-         (real-locate (string-match "\\`locate" helm-locate-command))
-         (case-sensitive-flag (if locate-is-es "-i" ""))
-         (ignore-case-flag (if (or locate-is-es
-                                   (not real-locate)) "" "-i"))
-         process-connection-type
-         (args (split-string helm-pattern " ")))
-    (prog1
-        (start-process-shell-command
-         "locate-process" helm-buffer
-         (format helm-locate-command
-                 (cl-case helm-locate-case-fold-search
-                   (smart (let ((case-fold-search nil))
-                            (if (string-match "[A-Z]" helm-pattern)
-                                case-sensitive-flag
-                              ignore-case-flag)))
-                   (t (if helm-locate-case-fold-search
-                          ignore-case-flag
-                        case-sensitive-flag)))
-                  ;; The pattern itself.
-		 (shell-quote-argument (mapconcat 'identity args ".*"))))
-;;(shell-quote-argument "dmxc.*capturing")))
-;;		  (replace-regexp-in-string " " ".*" args)))
-;;                 (concat
-;;                  (shell-quote-argument (car args)) " "
-                  ;; Possible locate args added
-                  ;; after pattern, don't quote them.
-  ;;                (mapconcat 'identity (cdr args) " "))))
-      (set-process-sentinel
-       (get-buffer-process helm-buffer)
-       #'(lambda (_process event)
-           (if (string= event "finished\n")
-               (with-helm-window
-                 (setq mode-line-format
-                       '(" " mode-line-buffer-identification " "
-                         (:eval (format "L%s" (helm-candidate-number-at-point))) " "
-                         (:eval (propertize
-                                 (format "[Locate Process Finish- (%s results)]"
-                                         (max (1- (count-lines
-                                                   (point-min) (point-max)))
-                                              0))
-                                 'face 'helm-locate-finish))))
-                 (force-mode-line-update))
-             (helm-log "Error: Locate %s"
-                       (replace-regexp-in-string "\n" "" event))))))))
-
-
-
-
-(defvar my-helm-source-locate
-  `((name . "Locate")
-    (init . helm-locate-set-command)
-    (candidates-process . my-new-helm-locate-init)
-    (type . file)
-    (requires-pattern . 3)
-    (history . ,'helm-file-name-history)
-    (keymap . ,helm-generic-files-map)
-    (help-message . helm-generic-file-help-message)
-    (candidate-number-limit . 9999)
-    (mode-line . helm-generic-file-mode-line-string))
-  "Find files matching the current input pattern with locate.")
-
-(defvar my-new-helm-source-locate
-  `((name . "Locate")
-    (init . helm-locate-set-command)
-    (candidates-process . my-new-helm-locate-init)
-    (type . file)
-    (requires-pattern . 3)
-    (history . ,'helm-file-name-history)
-    (keymap . ,helm-generic-files-map)
-    (help-message . helm-generic-file-help-message)
-    (candidate-number-limit . 9999)
-;;    (no-matchplugin)
-    (mode-line . helm-generic-file-mode-line-string))
-  "Find files matching the current input pattern with locate.")
-
+;; Show full file-path in helm result
+(setq helm-ff-transformer-show-only-basename nil)
 
 ;; file a file
 (global-set-key (kbd "C-x C-f")
@@ -180,7 +63,7 @@
      :prompt "Switch to: "
      :candidate-number-limit 25                 ;; up to 25 of each
      :sources
-     '(;;my-c-source-google-suggest
+     '(
 	   helm-source-files-in-current-dir ;; current dir
 		helm-c-source-recentf               ;; recent files
 ;;	   helm-source-find-files
