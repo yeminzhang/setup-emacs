79e2d229ec97e134a1ad8b5460982ef4054331a9
Yemin Zhang <yemin.zhang@ericsson.com>
2015-01-19 18:38:30 +0100

    smart copy line and delete line
    M-w: When no active mark, copy whole line without new line character,
    otherwise copy region.
    
    C-w: when no active mark, delete whole line including new line
    character, otherwise kill region

--- init/init-edit.el
+++ init/init-edit.el
@@ -125,6 +125,20 @@
 
 (add-hook 'c++-mode-hook 'show-trailing-whitespace)
 
+(defadvice kill-ring-save (before slick-copy activate compile)
+  "When called interactively with no active region, copy a single line instead."
+  (interactive (if mark-active (list (region-beginning) (region-end))
+                 (message "Copied line")
+                 (list (line-beginning-position)
+                       (line-end-position)))))
+
+(defadvice kill-region (before slick-cut activate compile)
+  "When called interactively with no active region, kill a single line instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (line-beginning-position)
+           (line-beginning-position 2)))))
+
 (set-language-environment "UTF-8")
 
 (provide 'init-edit)
