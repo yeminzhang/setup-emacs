51ed15e9fb3222943bd229a05cbd87a655faca18
Yemin Zhang <yemin.zhang@ericsson.com>
2015-03-09 15:58:02 +0100

    introduce command file-type
    choose a file using ido, and then print the
    file type in the echo area

--- init/init-misc.el
+++ init/init-misc.el
@@ -2,6 +2,10 @@
   (interactive)
   (message (current-time-string)))
 
+(defun file-type (filename)
+  (interactive (list (ido-read-file-name "file: ")))
+	  (shell-command (concat "file " filename)))
+
 (global-set-key (kbd "<f1>") 'print-time)
 
 (provide 'init-misc)
