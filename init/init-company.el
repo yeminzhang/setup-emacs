f8285ec43ac037eb15af5b09ea4a782400aebd54
Yemin Zhang <yemin.zhang@ericsson.com>
2015-03-10 20:01:02 +0100

    make company-mode more user friendly
    no idle delay. C-n/C-p to choose next/previous.
    And, use right-little-finger to select

--- init/init-company.el
+++ init/init-company.el
@@ -1,2 +1,6 @@
 (require 'company)
+(setq company-idle-delay 0)
+(define-key company-active-map (kbd right-little-finger-key) 'company-complete-selection)
+(define-key company-active-map (kbd "C-n") 'company-select-next-or-abort)
+(define-key company-active-map (kbd "C-p") 'company-select-previous-or-abort)
 (provide 'init-company)
