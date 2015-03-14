e69902712d42e55075be58d0fc926919cc593a6f
Yemin Zhang <yemin.zhang@ericsson.com>
2015-03-14 23:05:06 +0100

    Improve project compile & run & debug
    Now per project attributes can be saved automatically
    without manually editting the .dir-locals.el.
    
    With this advantage, project compile command can be saved
    automatically and persistent.
    
    project run & debug is now much more generic and comfortable.
    
    gdb debug is much better. breakpoints can be save automatically.
    
    Right now only gdb is supported by project-debug

--- init/init-c.el
+++ init/init-c.el
@@ -1,4 +1,3 @@
-(setq next-error-highlight t)
 ;;(require 'flymake-cursor)
 ;;(add-hook 'find-file-hook 'flymake-find-file-hook)
 
@@ -6,79 +5,41 @@
 (list "make"
 (list "-s" "-C" "./")))
 
-(require 'gud)
-(define-key gud-mode-map (kbd "C-q") 'debug-mode-quit)
 (setq gdb-speedbar-auto-raise t)
-(setq compilation-auto-jump-to-first-error t)
-(setq compilation-scroll-output 'first-error)
 
-(defun gud-active-process ()
-(if (get-buffer-process gud-comint-buffer) t nil)
+(defun cc-debug (executable parameters envs)
+  (interactive
+	  (list (ido-read-file-name "Executable: ")
+			(read-from-minibuffer "Parameters: ")
+			(read-from-minibuffer "Envs: ")
+			))
+  (setq command-line (concat "gdb -i=mi " executable)
+		gdb-many-windows t
+		gdb-show-main t)
+  (gdb command-line)
+  (setq-local project-executable-file executable)
+  (if parameters (gud-send-command (concat "set args " parameters)))
+  (if envs (let
+			 ((env-list (split-string envs ",")))
+			 (dolist (env env-list)
+			   (gud-send-command (concat "set environment " env)))))
+  (gdb-clear-inferior-io)
+  (if (file-exists-p (cc-breakpoints-filename))
+	  (progn
+		(gud-send-command (concat "source " (cc-breakpoints-filename)))
+		(gud-run 0)
+		))
+  )
+
+(defun cc-breakpoints-filename ()
+  (with-current-buffer "*gud-main*"
+  (concat "/tmp/" (s-replace "/" "!" project-executable-file))))
+
+(defun cc-debug-quit ()
+  (interactive ())
+  (gud-send-command (concat "save breakpoints " (cc-breakpoints-filename)))
+  (gud-send-command "quit")
 )
-
-(defun gud-send-command (command)
-(if (gud-active-process) (comint-send-string gud-comint-buffer (concat command "\n")))
-)
-
-(defun gud-save-window-configuration ()
-(setq window-configuration-before-gdb (current-window-configuration))
-)
-
-(defun gud-restore-window-configuration ()
-(set-window-configuration window-configuration-before-gdb)
-)
-
-(defun gud-save-debug-environment (command &optional parameters env-list)
-(setq gud-last-command command)
-(setq gud-last-parameters parameters)
-(setq gud-last-env-list env-list)
-)
-
-(defun debug-program (executable-file to-set-breakpoints &optional parameters envs)
-(interactive (list (ido-read-file-name "Executable: ")
-t
-(read-from-minibuffer "Parameters: ")
-(read-from-minibuffer "Envs: ")))
-(gud-save-window-configuration)
-(setq command-line (concat "gdb -i=mi " executable-file))
-(gdb command-line)
-(if to-set-breakpoints (gdb-many-windows nil))
-(if parameters (gud-send-command (concat "set args " parameters)))
-(if envs (progn
-(setq env-list (split-string envs ","))
-(dolist (env env-list)
-(gud-send-command (concat "set environment " env)))))
-(gdb-clear-inferior-io)
-(if to-set-breakpoints (gud-send-command "break main"))
-(gud-run 0)
-(gud-save-debug-environment command-line parameters env-list)
-)
-
-(defun redebug-program (to-enable-breakpoints)
-(interactive (list t))
-(progn
-(unless gdb-many-windows (gud-save-window-configuration))
-(unless (gud-active-process) (gdb gud-last-command))
-(with-current-buffer gud-comint-buffer
-(if to-enable-breakpoints 
-(progn  (gud-send-command "enable")
-(unless gdb-many-windows (gdb-many-windows nil))
-))
-(if gud-last-parameters (gud-send-command (concat "set args " gud-last-parameters)))
-(if gud-last-env-list 
-(dolist (env gud-last-env-list)
-(gud-send-command (concat "set environment " env))))
-(gdb-clear-inferior-io)
-(gud-run 0))))
-
-(defun debug-mode-quit ()
-(interactive ())
-(if gdb-many-windows (gdb-many-windows nil))
-(gud-restore-window-configuration)
-)
-
-;;(global-set-key (kbd "<f6>") 'rerun-program)
-(global-set-key (kbd "<f7>") 'redebug-program)
 
 ;; For C/C++ buffer local jump
 (defun cc-jump-local ()
--- init/init-project.el
+++ init/init-project.el
@@ -1,7 +1,87 @@
+(setq compilation-auto-jump-to-first-error t)
+(setq compilation-scroll-output 'first-error)
+(setq next-error-highlight t)
+
+(require 'gud)
+(define-key gud-mode-map (kbd "C-q") 'project-debug-quit)
+
+(defun project-save-attribute (symbol value)
+  (let (
+		(default-directory (projectile-project-root))
+		(buffer-name (current-buffer))
+		)
+	(add-dir-local-variable nil symbol value)
+	(save-buffer)
+	(bury-buffer)
+	(set-buffer buffer-name)
+	))
+
+(defun project-set-running-command ()
+  (project-save-attribute 'project-executable-file (ido-read-file-name "Executable: "))
+  (project-save-attribute 'project-executable-parameters (read-from-minibuffer "Parameters: "))
+  (project-save-attribute 'project-executable-envs (read-from-minibuffer "Envs: "))
+  )
+
+(defun project-load-attributes()
+  (hack-dir-local-variables)
+  (dolist (pair dir-local-variables-alist)
+	(set (make-local-variable (car pair)) (cdr pair)))
+)
+
+(defun project-run (ARG)
+  (interactive "P")
+  (project-load-attributes)
+  (if (or ARG (not (boundp 'project-executable-file)))
+	  (project-set-running-command)
+	)
+  (project-load-attributes)
+  (shell-command (concat project-executable-file " " project-executable-parameters)))
+
+(defun project-debug (ARG)
+  (interactive "P")
+  (project-load-attributes)
+  (if (not (boundp 'project-debug-program))
+	  (project-save-attribute 'project-debug-program (ido-completing-read "Debugger: " (list "gdb"))))
+	(if (or ARG (not (boundp 'project-executable-file)))
+		(project-set-running-command)
+	  )
+	(project-load-attributes)
+	(gud-save-window-configuration)
+	(if (string= project-debug-program "gdb") (cc-debug project-executable-file project-executable-parameters project-executable-envs))
+	)
+
+(defun project-debug-quit ()
+  (interactive ())
+  (if (eq gud-minor-mode 'gdbmi) (cc-debug-quit))
+  (gud-restore-window-configuration)
+  (bury-buffer "*gud-main*")
+  )
+
+(defun gud-active-process ()
+  (if (get-buffer-process gud-comint-buffer) t nil)
+  )
+
+(defun gud-send-command (command)
+  (if (gud-active-process)
+	  (comint-send-string gud-comint-buffer (concat command "\n"))
+	))
+
+(defun gud-save-window-configuration ()
+  (setq window-configuration-before-gdb (current-window-configuration))
+  )
+
+(defun gud-restore-window-configuration ()
+  (set-window-configuration window-configuration-before-gdb)
+  )
+
 (defun project-compile (ARG)
   (interactive "P")
   (save-some-buffers 1)
-  (projectile-compile-project ARG))
+  (project-load-attributes)
+  (if (boundp 'project-compile-command)
+	  (puthash (projectile-project-root) project-compile-command projectile-compilation-cmd-map))
+  (projectile-compile-project ARG)
+  (project-save-attribute 'project-compile-command (gethash (projectile-project-root) projectile-compilation-cmd-map)))
 
 ;; Close the compilation window if there was no error at all.
 (setq compilation-exit-message-function
@@ -29,17 +109,6 @@
 	(if (string= "erlang-mode" major-mode) (erlang-find-tag-under-point))
 	(if (string= "emacs-lisp-mode" major-mode) (elisp-find-function-under-point))
 	))
-
-;; The limitation is that there is only one run-command globally,
-;; not per project. Don't like directory local variables. Hope
-;; projectile can support this in the future by per project conf.
-(defun project-run (ARG)
-  (interactive "P")
-  (if (or ARG (not (boundp 'project-executable-file)))
-	  (setq project-executable-file (ido-read-file-name "Executable: ")
-			project-executable-parameters (read-from-minibuffer "Parameters: ")
-			))
-  (shell-command (concat project-executable-file " " project-executable-parameters)))
 
 (require 'helm-gtags)
 
@@ -77,6 +146,7 @@
 (setq compilation-read-command nil)
 (global-set-key (kbd "<f5>") 'project-compile)
 (global-set-key (kbd "<f6>") 'project-run)
+(global-set-key (kbd "<f7>") 'project-debug)
 (setq projectile-mode-line '(:eval (format " Proj[%s]" (projectile-project-name))))
 (define-key projectile-mode-map (kbd "C-c p g") 'helm-projectile-grep)
 (setq projectile-find-dir-includes-top-level t)
