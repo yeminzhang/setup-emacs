67dc19cd075ec494dba7ce4d67b6bf2c281a4bd0
Yemin Zhang <yemin.zhang@ericsson.com>
2015-04-10 13:45:52 +0200

    Use custom.el as custom-file instead of init.el

--- init/init-env.el
+++ init/init-env.el
@@ -2,6 +2,10 @@
 (if (file-exists-p (expand-file-name "env.el" user-emacs-directory))
 	(load (expand-file-name "env.el" user-emacs-directory)))
 
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(if (file-exists-p custom-file)
+	(load custom-file))
+
 ;; set right-little-finger-key based on keyboard layout
 (if (and (boundp 'keyboard-layout) (string= keyboard-layout "sv"))
     (defconst right-little-finger-key "ö")
