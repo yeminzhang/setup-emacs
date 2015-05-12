221c05ef0c37b5d1bda8ddd52d1818db98190bf0
Yemin Zhang <yemin.zhang@ericsson.com>
2015-05-12 22:37:07 +0200

    Use custom.el to customize env
    Now user no longer needs to manually edit env.el.
    When emacs starts for the first time, user will be
    prompted to configure that. Those configuration will
    be saved as customized variables in custom.el.
    
    So env-default.el and env.el are no longer used.
    Now env-default is kept for a while, until ede project
    config can be automated in the future

--- init/init-env.el
+++ init/init-env.el
@@ -1,22 +1,42 @@
-(load (expand-file-name "env-default.el" user-emacs-directory))
-(if (file-exists-p (expand-file-name "env.el" user-emacs-directory))
-	(load (expand-file-name "env.el" user-emacs-directory)))
+(require 'tramp)
+
+;; default value
+(setq *is-sv-kbmap* t)
+(setq ssh-tunnel-host-list '())
+
+(defun env-customize ()
+  (interactive)
+  (env-set-keyboard-layout)
+  (env-set-ssh-tunnel-list))
+
+(defun env-set-keyboard-layout()
+  (interactive)
+  (if (y-or-n-p "Do you use Swedish Keyboard layout? ")
+    (customize-save-variable '*is-sv-kbmap* t)
+    (customize-save-variable '*is-sv-kbmap* nil)))
+
+(defun env-set-ssh-tunnel-list()
+  (interactive)
+  (let (
+	(sconfig-list (tramp-parse-sconfig "~/.ssh/config"))
+	(ssh-tunnel-list ()))
+    (dolist (login sconfig-list)
+      (if (and login (nth 1 login) (y-or-n-p (concat "Add " (nth 1 login) " to ssh tunnel list? ")))
+		  (add-to-list 'ssh-tunnel-list (nth 1 login) t)))
+    (customize-save-variable 'ssh-tunnel-host-list ssh-tunnel-list)))
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(if (file-exists-p custom-file)
-	(load custom-file))
+(unless (file-exists-p custom-file)
+  (env-customize))
+
+(load custom-file)
 
 ;; set right-little-finger-key based on keyboard layout
-(if (and (boundp 'keyboard-layout) (string= keyboard-layout "sv"))
-    (defconst *is-sv-kbmap* t)
-  (defconst *is-sv-kbmap* nil))
-
 (if *is-sv-kbmap*
-    (defconst right-little-finger-key "ö")
+	(defconst right-little-finger-key "ö")
   (defconst right-little-finger-key ";"))
-
 (if *is-sv-kbmap*
-    (defconst bottom-right-finger-key "-")
+	(defconst bottom-right-finger-key "-")
   (defconst bottom-right-finger-key "/"))
 
 ;; OS
