bbdb64ffd74aa9034d274b5cf44b5a539e3b20e9
Yemin Zhang <yemin.zhang@ericsson.com>
2015-07-20 16:32:31 +0200

    make erlang-mode use helm-gtags
    instead of erlang-find-tag. ctags works pretty well for
    erlang except that it doesn't support module:function.

--- init/init-erlang.el
+++ init/init-erlang.el
@@ -9,19 +9,4 @@
 
 (add-hook 'erlang-mode-hook #'(lambda () (company-mode 1)))
 
-(defun dmx-reload ()
-(interactive)
-(shell-command "scp ~/dmx/dmxc/out/opt/dmxc/lib/erlang/lib/dmxc-4.0/ebin/* root@SC-2-1:/persistent/dmx/active/dmxc/dev_patches/" nil)
-(erl-reload-modules "dmxc1@SC-2-1"))
-
-(defun erlang-find-tag-under-point ()
-(interactive)
-(let
-    ((project-id (project-get-id buffer-file-name)))
-  (if project-id 
-      (progn
-	(setq tags-file-name (concat (project-get-attribute project-id :root-dir) "TAGS"))
-	(erlang-find-tag (erlang-find-tag-default))
-	))))
-
 (provide 'init-erlang)
