00c0c879b5696f3d781fbd241b0cf7fcfa1aeaaf
Yemin Zhang <yemin.zhang@ericsson.com>
2015-09-05 17:25:52 +0200

    Use 2 spaces to indent elisp code
    4 seems too much...

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -1,24 +1,24 @@
 (add-hook 'emacs-lisp-mode-hook #'(lambda () (company-mode 1)))
 (add-hook 'emacs-lisp-mode-hook #'(lambda () (setq-local indent-line-function 'elisp-indent-line)))
 
-(setq lisp-body-indent 4)
+(setq lisp-body-indent 2)
 
 (defun elisp-indent-line ()
-	(interactive)
-	(beginning-of-line)
-	(delete-horizontal-space)
-	(lisp-indent-line))
+  (interactive)
+  (beginning-of-line)
+  (delete-horizontal-space)
+  (lisp-indent-line))
 
 (defun elisp-find-function-under-point ()
-(interactive)
-(find-function (function-called-at-point)))
+  (interactive)
+  (find-function (function-called-at-point)))
 
 (defun elisp-save-and-eval-buffer ()
-(interactive)
-(progn
-(save-buffer)
-(eval-buffer)
-))
+  (interactive)
+  (progn
+	(save-buffer)
+	(eval-buffer)
+	))
 
 (define-key emacs-lisp-mode-map (kbd "<f6>") 'elisp-save-and-eval-buffer)
 (define-key emacs-lisp-mode-map (kbd "<tab>") 'helm-yas-complete)
