909f07b133e8e213414018fb49026de6dd519881
Yemin Zhang <yemin.zhang@ericsson.com>
2015-09-08 11:22:05 +0200

    Don't load helm until it is needed
    For init-helm, after-load doesn't make sense
    here because it is actually needed at the
    beginning of emacs boot

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -1,11 +1,14 @@
 (require-packages '(helm))
+
+(after-load 'helm
+  (setq helm-idle-delay 0.01)
+  (setq helm-input-idle-delay 0.01)
+  (define-key helm-map (kbd right-little-finger-key) 'helm-maybe-exit-minibuffer)
+  (setq helm-completion-mode-string "")
+  (setq helm-full-frame nil)
+  (setq helm-buffers-fuzzy-matching t))
+
 (require 'helm-config)
 (helm-mode 1)
-(setq helm-idle-delay 0.01)
-(setq helm-input-idle-delay 0.01)
-(define-key helm-map (kbd right-little-finger-key) 'helm-maybe-exit-minibuffer)
-(setq helm-completion-mode-string "")
-(setq helm-full-frame nil)
-(setq helm-buffers-fuzzy-matching t)
 
 (provide 'init-helm)
