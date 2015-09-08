b2ce2fced1365348bd223a72d62d06a256c38ebf
Yemin Zhang <yemin.zhang@ericsson.com>
2015-09-08 17:15:18 +0200

    Don't load python until it is needed

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,20 +1,20 @@
 (require-packages '(company-jedi))
-(require 'python)
-(require 'company-jedi)
+
+(after-load 'python
+  ;; Now company-jedi seems to work only under python2
+  (define-key python-mode-map (kbd "M-.") 'jedi:goto-definition)
+  (define-key python-mode-map (kbd "M-,") 'jedi:goto-definition-pop-marker)
+  (require 'company-jedi))
 
 (after-load 'company
   (add-to-list 'company-backends 'company-jedi))
 
 (add-hook 'python-mode-hook
-		  #'(lambda ()
-			  (company-mode 1)
-			  (local-set-key (kbd "C-j") 'helm-code-select)
-			  (local-set-key (kbd "C-c <") 'helm-gtags-previous-history)
-			  (local-set-key (kbd "C-c >") 'helm-gtags-next-history)
-			  ))
-
-;; Now company-jedi seems to work only under python2
-(define-key python-mode-map (kbd "M-.") 'jedi:goto-definition)
-(define-key python-mode-map (kbd "M-,") 'jedi:goto-definition-pop-marker)
+          (lambda ()
+            (company-mode 1)
+            (local-set-key (kbd "C-j") 'helm-code-select)
+            (local-set-key (kbd "C-c <") 'helm-gtags-previous-history)
+            (local-set-key (kbd "C-c >") 'helm-gtags-next-history)
+            ))
 
 (provide 'init-python)
