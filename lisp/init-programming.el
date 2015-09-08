ee7131f3f3e5bfc7f994eded62e1cdd2c969a978
Yemin Zhang <yemin.zhang@ericsson.com>
2015-09-08 12:48:41 +0200

    Don't load helm-gtags and helm-semantics until needed
    yas and semantic minor mode will be loaded during
    emacs boots

--- lisp/init-programming.el
+++ lisp/init-programming.el
@@ -5,55 +5,51 @@
   (shell-command (concat "readelf -a " filename)))
 
 ;; configure semantic
-(require 'semantic)
+(semantic-mode 1)
 (global-semanticdb-minor-mode 1)
 (global-semantic-idle-scheduler-mode 1)
-(semantic-mode 1)
-(require 'helm-semantic)
 
 (defun get-mode-map (mode)
   (if (eq mode 'cc-mode) 'c++-mode-map)
   )
 
 ;; Package: yasnippet
-(require 'yasnippet)
+(after-load 'yasnippet
+  (define-key yas-minor-mode-map [backtab] 'yas-expand)
+  (define-key yas-minor-mode-map [(tab)]        nil)
+  (define-key yas-minor-mode-map (kbd "TAB")    nil)
+  (define-key yas-minor-mode-map (kbd "<tab>")  nil))
 (yas-global-mode 1)
 
-(define-key yas-minor-mode-map [backtab] 'yas-expand)
-
-(define-key yas-minor-mode-map [(tab)]        nil)
-(define-key yas-minor-mode-map (kbd "TAB")    nil)
-(define-key yas-minor-mode-map (kbd "<tab>")  nil)
-
-(require 'helm-gtags)
-
-(setq
- helm-gtags-ignore-case t
- helm-gtags-auto-update t
- helm-gtags-use-input-at-cursor t
- helm-gtags-pulse-at-cursor t
- helm-gtags-prefix-key "\C-cg"
- helm-gtags-suggested-key-mapping t
- helm-gtags-mode-name " HG"
- )
-
-;; key bindings
-(define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
-(define-key helm-gtags-mode-map (kbd "C-j") 'helm-code-select)
-(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
-(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
-(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
-(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+;; helm-gtags
+(after-load 'helm-gtags
+  (setq
+   helm-gtags-ignore-case t
+   helm-gtags-auto-update t
+   helm-gtags-use-input-at-cursor t
+   helm-gtags-pulse-at-cursor t
+   helm-gtags-prefix-key "\C-cg"
+   helm-gtags-suggested-key-mapping t
+   helm-gtags-mode-name " HG"
+   )
+  ;; key bindings
+  (define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+  (define-key helm-gtags-mode-map (kbd "C-j") 'helm-code-select)
+  (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+  (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+  (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+  (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history))
 
 (defun helm-code-select ()
   (interactive)
+  (require 'helm-semantic)
   (helm
    :prompt "Go to: "
    :candidate-number-limit 9999
    :sources
    '(
-	 helm-source-semantic
-	 helm-source-gtags-select)))
+     helm-source-semantic
+     helm-source-gtags-select)))
 
 (defun format-buffer ()
   (interactive)
