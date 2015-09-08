fa58c1fe43b53fc304f795e8e564bfe27a14092b
Yemin Zhang <yemin.zhang@ericsson.com>
2015-09-08 11:11:20 +0200

    chinese-pyim will be not be loaded until needed

--- lisp/init-chinese.el
+++ lisp/init-chinese.el
@@ -1,13 +1,18 @@
 (require-packages '(chinese-pyim pinyin-search))
-(require 'chinese-pyim)
 
-(setq default-input-method "chinese-pyim"
-	  pyim-dicts  '((:name "bigdict" :file "~/.emacs.d/pyim/pyim-bigdict.txt" :coding utf-8-unix))
-	  pyim-use-tooltip t)
+(setq default-input-method "chinese-pyim")
 
-(define-key pyim-mode-map (kbd ".")   'pyim-next-page)
-(define-key pyim-mode-map (kbd ",")   'pyim-previous-page)
-(global-set-key (kbd "C-S-SPC") 'toggle-input-method)
+(after-load 'chinese-pyim-core
+  (setq pyim-dicts  '((:name "bigdict" :file "~/.emacs.d/pyim/pyim-bigdict.txt" :coding utf-8-unix)))
+  (setq pyim-use-tooltip t)
+  (define-key pyim-mode-map (kbd ".") 'pyim-next-page)
+  (define-key pyim-mode-map (kbd ",") 'pyim-previous-page))
+
+(global-set-key (kbd "C-S-SPC")
+                (lambda ()
+                  (interactive)
+                  (require 'chinese-pyim)
+                  (toggle-input-method)))
 
 (global-set-key (kbd "C-S-s") 'isearch-forward-pinyin)
 (global-set-key (kbd "C-S-r") 'isearch-backward-pinyin)
