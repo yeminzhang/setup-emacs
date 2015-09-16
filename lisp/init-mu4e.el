7a32a3516b8ffddac3e422efb5cccf65611f85d0
Yemin Zhang <yemin.zhang@ericsson.com>
2015-09-16 13:35:32 +0200

    fix a bug in init-mu4e
    variable name has changed from mu4e~xx to mu4e-xx

--- lisp/init-mu4e.el
+++ lisp/init-mu4e.el
@@ -81,16 +81,18 @@
     ))
 
 (defun my-mu4e-headers-update()
-  (with-current-buffer mu4e~headers-buffer-name
-    (setq maxnum (unless mu4e-headers-full-search mu4e-headers-results-limit))
-    (mu4e~proc-find
-     mu4e~headers-last-query
-     mu4e-headers-show-threads
-     mu4e~headers-sort-field
-     mu4e~headers-sort-direction
-     maxnum
-     mu4e-headers-skip-duplicates
-     mu4e-headers-include-related)))
+  (let (
+        (maxnum (unless mu4e-headers-full-search mu4e-headers-results-limit))
+        )
+    (with-current-buffer mu4e~headers-buffer-name
+      (mu4e~proc-find
+       mu4e~headers-last-query
+       mu4e-headers-show-threads
+       mu4e-headers-sort-field
+       mu4e-headers-sort-direction
+       maxnum
+       mu4e-headers-skip-duplicates
+       mu4e-headers-include-related))))
 
 (defun my-mu4e-open(ARG)
   (interactive "P")
