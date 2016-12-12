2ee90c33d1076bae5648f746a7b45fb6025ec107
yemin <yemin@mail.atzweb.com>
2016-12-12 10:19:50 +0800

    Revert "use RETURN to select in company mode"
    This reverts commit b75633ec67b3f949d0088d395285d1338db19f63.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -2,6 +2,7 @@
 
 (after-load 'company
   (setq company-idle-delay 0)
+  (define-key company-active-map (kbd right-little-finger-key) 'company-complete-selection)
   (define-key company-active-map (kbd "C-n") 'company-select-next-or-abort)
   (define-key company-active-map (kbd "C-p") 'company-select-previous-or-abort))
 
