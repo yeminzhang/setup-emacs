c031ef4cda09fffce8b3f0fd3ecf583f05d29194
Yemin Zhang <yeminzhang_sh@163.com>
2017-06-02 20:52:36 +0800

    change vhl background to a milder color
    just a little bit milder...

--- lisp/init-appearance.el
+++ lisp/init-appearance.el
@@ -84,7 +84,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(vhl/default-face ((t (:inherit secondary-selection :background "tan" :foreground "black")))))
+ '(vhl/default-face ((t (:inherit secondary-selection :background "DarkKhaki" :foreground "black")))))
 
 (use-package smart-mode-line
   :ensure t
