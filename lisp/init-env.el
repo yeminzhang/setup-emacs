0c772437f99f7281b9835fa5c3f9ac946e66c689
yemin <yemin@mail.atzweb.com>
2017-06-28 12:05:51 +0800

    allow customized exec-path based on customized $PATH in custom.el
    PATH env can be set in custom.el. Then exec-path will be updated based
    on that.

--- lisp/init-env.el
+++ lisp/init-env.el
@@ -5,4 +5,8 @@
 (defconst *is-linux* (eq system-type 'gnu/linux))
 (defconst *is-mac* (eq system-type 'darwin))
 
+;; set exec-path from $PATH env. $PATH env can be modified in custom.el
+(setq exec-path (split-string (getenv "PATH") ":"))
+(add-to-list 'exec-path exec-directory t)
+
 (provide 'init-env)
